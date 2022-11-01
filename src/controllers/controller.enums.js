584c66da352bc70438256520205c67902abea6fe
raveenaw_IT19146652 <it19146652@my.sliit.lk>
2022-11-01 13:27:52 +0530

    Implemented enums

---
+++ src/controllers/controller.enums.js
@@ -0,0 +1,30 @@
+export default {
+  role: {
+    WORKER: 'WORKER',
+    ADMIN: 'ADMIN',
+    MANAGER: 'MANAGER',
+  },
+  roleIssue: {
+    ONLY_WORKER: 'Only workers can asscess this function',
+    ONLY_ADMIN: 'Only admin can access this function',
+    ONLY_MANAGER: 'Only Managers  can access this function',
+  },
+  message: {
+    NOT_FOUND: 'Message is not found',
+  },
+  user: {
+    CREATE_SUCCESS: 'User created successfully',
+    CREATE_ERROR: 'User creation unsuccess',
+    LOGIN_SUCCESS: 'User login success',
+    LOGIN_ERROR: 'User login unsuccess',
+    ALREADY_EXIST: 'User already exists',
+    NOT_FOUND: 'User not found',
+    PASSWORD_NOT_MATCH: 'Password is not matched',
+    CREDENTIAL_REQUIRED: 'Username and the password is required',
+  },
+  message: {
+    CREATE_SUCCESS: 'Message saved successfully',
+    CREATE_ERROR: 'Message saving unsuccess',
+    NOT_FOUND: 'Message not found',
+  },
+};
