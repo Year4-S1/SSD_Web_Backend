5e895f95940332d1c4c7ffeea9b93f700f442010
raveenaw_IT19146652 <it19146652@my.sliit.lk>
2022-11-02 20:52:13 +0530

    Save message function implemented

--- src/services/message-service.js
+++ src/services/message-service.js
@@ -1,4 +1,29 @@
 const express = require("express");
-const Message = require("../models/Message");
-const enums = require("../controllers/message.enums");
+const Message = require("../models/message-model");
+const enums = require("../enums/message-enums");
+const LOG = require("../log/log");
+const responseHandler = require("../response/response-handler");
+
+const saveMessage = async (req, res) => {
+  if (req.body) {
+    const message = new Message();
+    message.createdBy = req.body.createdBy;
+    message.title = req.body.title;
+    message.message = req.body.message;
+    message.messageDate = new Date().toLocaleDateString();
+    message.messageTime = new Date().toTimeString();
+    await message
+      .save()
+      .then((data) => {
+        res.status(200).send({ data: data });
+      })
+      .catch((error) => {
+        res.status(500).send({ error: error.message });
+      });
+  }
+};
+
+module.exports = {
+  saveMessage
+}
 
